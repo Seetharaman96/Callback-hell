a494884b5ec901749a5f9cb8f993ca17917dd375
Seetharaman96 <seetharam.ad96@gmail.com>
2023-01-03 19:34:45 +0530

    upload

---
+++ script.js
@@ -0,0 +1,55 @@
+setTimeout(() => {
+    var upload = document.querySelector('.countDown')
+    upload.innerHTML = `<h1>${10}</h1>`
+    document.body.style.backgroundColor = 'red'
+    setTimeout(() => {
+        var upload = document.querySelector('.countDown')
+        upload.innerHTML = `<h1>${9}</h1>`
+        document.body.style.backgroundColor = 'green'
+        setTimeout(() => {
+            var upload = document.querySelector('.countDown')
+            upload.innerHTML = `<h1>${8}</h1>`
+            document.body.style.backgroundColor = 'yellow'
+            setTimeout(() => {
+                var upload = document.querySelector('.countDown')
+                upload.innerHTML = `<h1>${7}</h1>`
+                document.body.style.backgroundColor = 'orange'
+                setTimeout(() => {
+                    var upload = document.querySelector('.countDown')
+                    upload.innerHTML = `<h1>${6}</h1>`
+                    document.body.style.backgroundColor = 'aquamarine'
+                    setTimeout(() => {
+                        var upload = document.querySelector('.countDown')
+                        upload.innerHTML = `<h1>${5}</h1>`
+                        document.body.style.backgroundColor = 'pink'
+                        setTimeout(() => {
+                            var upload = document.querySelector('.countDown')
+                            upload.innerHTML = `<h1>${4}</h1>`
+                            document.body.style.backgroundColor = 'darkgreen'
+                            setTimeout(() => {
+                                var upload = document.querySelector('.countDown')
+                                upload.innerHTML = `<h1>${3}</h1>`
+                                document.body.style.backgroundColor = 'purple'
+                                setTimeout(() => {
+                                    var upload = document.querySelector('.countDown')
+                                    upload.innerHTML = `<h1>${2}</h1>`
+                                    document.body.style.backgroundColor = 'gray'
+                                    setTimeout(() => {
+                                        var upload = document.querySelector('.countDown')
+                                        upload.innerHTML = `<h1>${1}</h1>`
+                                        document.body.style.backgroundColor = 'indigo'
+                                        setTimeout(() => {
+                                            var upload = document.querySelector('.countDown')
+                                            upload.innerHTML = `<h1>${'Happy Independence Day'}</h1>`
+                                            document.body.style.backgroundColor = 'aqua'
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
